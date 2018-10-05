c47a2103d15516c45ff24b4a0c614c843e86a5b5
Luigi A. A. Sansonetti <luigi.sansonetti@epfl.ch>
2018-10-05 15:26:03 +0200

    Worked a bit on magician_coin. Still doesn't quite work

--- PotW/Week 2/magician_coin.cpp
+++ PotW/Week 2/magician_coin.cpp
@@ -3,8 +3,57 @@
 #include <iomanip>
 #include <cmath>
 
-bool sort_by_first(std::pair<double, int> a, std::pair<double, int> b) {
-    return a.first >= b.first;
+double dee_pee(std::vector<double> &p, int day, int k_today, int m, int n, std::vector<std::vector<double> > *memo) {
+    if (k_today >= m) {
+        // You done, fam!
+        return 1;
+    }
+    if (k_today == 0 || day == n) {
+        // You ruined, fam!
+        return 0;
+    }
+
+    double p_today = p[day];
+    int tomorrow = day + 1;
+    if (memo->at(day)[0] == -1) {
+        memo->at(day)[0] = dee_pee(p, tomorrow, k_today, m, n, memo);
+    }
+    double max = memo->at(day)[0];
+
+    for (int i = 1 ; i <= k_today ; i++) {
+        if (memo->at(day)[i] == -1) {
+            memo->at(day)[i] = p_today * dee_pee(p, tomorrow, k_today + i, m, n, memo) +
+                (1 - p_today) * dee_pee(p, tomorrow, k_today - i, m, n, memo);
+        }
+        max = std::max(max, memo->at(day)[i]);
+    }
+
+    return max;
+}
+
+// Dis shit cuz dis ain't dee-pee, fam!
+double do_the_thing(std::vector<double> &p, int day, int k_today, int m, int n) {
+    if (k_today >= m) {
+        // You done, fam!
+        return 1;
+    }
+    if (k_today == 0 || day == n) {
+        // You ruined, fam!
+        return 0;
+    }
+
+    double p_today = p[day];
+    int tomorrow = day + 1;
+    double max = do_the_thing(p, tomorrow, k_today, m, n);
+
+    for (int i = 1 ; i <= k_today ; i++) {
+        max = std::max(max,
+                p_today * do_the_thing(p, tomorrow, k_today + i, m, n) +
+                (1 - p_today) * do_the_thing(p, tomorrow, k_today - i, m, n)
+                );
+    }
+
+    return max;
 }
 
 void testcase() {
@@ -12,39 +61,21 @@ void testcase() {
     int k; std::cin >> k; // Initial wealth
     int m; std::cin >> m; // Target wealth
 
-    std::vector<double> p(n); // Probabilities of winning, per day
+    std::vector<double> p(n);
     for (int i = 0 ; i < n ; i++) {
         std::cin >> p[i];
     }
 
-    // BEST STRATEGIES
-    // Sorting the days by decreasing winning probability
-    std::vector<std::pair<double, int> > prob_day(n);
-    for (int i = 0 ; i < n ; i++) {
-        prob_day[i] = std::make_pair(p[i], i);
-    }
-    std::sort(prob_day.begin(), prob_day.end(), sort_by_first);
-    
-    // Getting the best strategies.
-    int potential_gain = k;
-    int i = 1;
-    while (potential_gain < m) {
-        potential_gain *= 2;
-        i++;
-    }
-
-    // TRY & RECOVER
-    // TODO
-    
+    std::vector<std::vector<double> > memo(n, std::vector<double>(m, -1));
 
     std::cout << std::fixed << std::setprecision(5);
-    std::cout << "lol" << std::endl;
+    std::cout << dee_pee(p, 0, k, m, n, &memo) << std::endl;
 }
 
 int main() {
     int t; std::cin >> t;
     while (t--) {
-        testcase();
+        testcase() ;
     }
 
     return 0;
