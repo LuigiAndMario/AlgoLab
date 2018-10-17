d5ac4a873159f86f7a0984722a3833a1174e42c4
Luigi A. A. Sansonetti <luigi.sansonetti@epfl.ch>
2018-10-17 16:04:18 +0200

    finished important_bridges

--- Week 4/important_bridges/important_bridges.cpp
+++ Week 4/important_bridges/important_bridges.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <set>
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/biconnected_components.hpp>
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::allow_parallel_edge_tag, boost::property<boost::edge_weight_t, int> > Graph;
+// Copied-pasted from the doc - why the hell do they use magic number like that though ?
+struct edge_component_t {
+    enum {
+        num = 555
+    };
+    typedef boost::edge_property_tag kind;
+} edge_component;
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::no_property, boost::property<edge_component_t, int> > Graph;
 typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
 typedef boost::graph_traits<Graph>::vertex_iterator VertexIterator;
 typedef boost::graph_traits<Graph>::edge_descriptor Edge;
 typedef boost::graph_traits<Graph>::edge_iterator EdgeIterator;
+typedef boost::property_map<Graph, edge_component_t>::type ComponentMap;
 
-bool my_sort(std::pair<Vertex, Vertex> e_i, std::pair<Vertex, Vertex> e_j) {
+bool weird_sort(std::pair<Vertex, Vertex> e_i, std::pair<Vertex, Vertex> e_j) {
     return e_i.first == e_j.first ? e_i.second < e_j.second : e_i.first < e_j.first;
 }
 
@@ -33,38 +41,29 @@ void testcase() {
         tie(ed, success) = boost::add_edge(i_1, i_2, archipelago);
     }
 
-    std::vector<Vertex> art_pts;
-    articulation_points(archipelago, back_inserter(art_pts));
+    ComponentMap edge_components = get(edge_component, archipelago);
+    int bcc = biconnected_components(archipelago, edge_components);
+    std::vector<std::vector<std::pair<int, int> > > bridges_per_component(m);
 
-    std::vector<std::pair<Vertex, Vertex> > critical_bridges;
-    std::set<std::pair<Vertex, Vertex> > critical_bridges_set;
-    EdgeIterator ei;
+    EdgeIterator i;
     EdgeIterator end;
+    for (tie(i, end) = boost::edges(archipelago) ; i != end ; i++) {
+        bridges_per_component[edge_components[*i]].push_back(std::make_pair(source(*i, archipelago), target(*i, archipelago)));
+    }
 
-    for (std::vector<Vertex>::iterator vi = art_pts.begin() ; vi != art_pts.end() ; vi++) {
-        for (tie(ei, end) = edges(archipelago) ; ei != end ; ei++) {
-            Vertex i_1 = source(*ei, archipelago);
-            Vertex i_2 = target(*ei, archipelago);
-
-            if (*vi == i_1 || *vi == i_2) {
-                critical_bridges_set.insert(i_1 < i_2 ? std::make_pair(i_1, i_2) : std::make_pair(i_2, i_1));
-            }
+    std::vector<std::pair<int, int> > important_bridges;
+    for (int i = 0 ; i < bcc ; i++) {
+        if (bridges_per_component[i].size() == 1) {
+            // important bridge
+            important_bridges.push_back(adjust(bridges_per_component[i][0]));
         }
     }
 
-    critical_bridges = std::vector<std::pair<Vertex, Vertex> >(critical_bridges_set.begin(), critical_bridges_set.end());
-    std::sort(critical_bridges.begin(), critical_bridges.end(), my_sort);
-    // critical_bridges.erase(unique(critical_bridges.begin(), critical_bridges.end()), critical_bridges.end());
-    int k = critical_bridges_set.size();
+    std::sort(important_bridges.begin(), important_bridges.end(), weird_sort);
 
-    std::cout << k << std::endl;
-    /*
-    for (int i = 0 ; i < k ; i++) {
-        std::cout << critical_bridges[i].first << " " << critical_bridges[i].second << std::endl;
-    }
-    */
-    for (std::set<std::pair<Vertex, Vertex> >::iterator bridge = critical_bridges_set.begin() ; bridge != critical_bridges_set.end() ; bridge++) {
-        std::cout << bridge->first << " " << bridge->second << std::endl;
+    std::cout << important_bridges.size() << std::endl;
+    for (int i = 0 ; i < important_bridges.size() ; i++) {
+        std::cout << important_bridges[i].first << " " << important_bridges[i].second << std::endl;
     }
 }
 
