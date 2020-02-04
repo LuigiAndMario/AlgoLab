5af288544d78588733b6520c4d81e91a7a769833
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2020-02-04 09:52:50 +0100

    Tour of gaul at 70

---
+++ week12/tourofgaul/tourofgaul_1.cpp
@@ -0,0 +1,108 @@
+// Includes
+// ========
+#include <iostream>
+#include <vector>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+boost::property<boost::edge_capacity_t, long,
+boost::property<boost::edge_residual_capacity_t, long,
+boost::property<boost::edge_reverse_t, traits::edge_descriptor,
+boost::property <boost::edge_weight_t, long> > > > > graph; // new! weightmap corresponds to costs
+
+typedef boost::graph_traits<graph>::edge_descriptor             edge_desc;
+typedef boost::graph_traits<graph>::out_edge_iterator           out_edge_it; // Iterator
+
+// Custom edge adder class
+class edge_adder {
+    graph &G;
+    
+public:
+    explicit edge_adder(graph &G) : G(G) {}
+    void add_edge(int from, int to, long capacity, long cost) {
+        auto c_map = boost::get(boost::edge_capacity, G);
+        auto r_map = boost::get(boost::edge_reverse, G);
+        auto w_map = boost::get(boost::edge_weight, G); // new!
+        const edge_desc e = boost::add_edge(from, to, G).first;
+        const edge_desc rev_e = boost::add_edge(to, from, G).first;
+        c_map[e] = capacity;
+        c_map[rev_e] = 0; // reverse edge has no capacity!
+        r_map[e] = rev_e;
+        r_map[rev_e] = e;
+        w_map[e] = cost;   // new assign cost
+        w_map[rev_e] = -cost;   // new negative cost
+    }
+};
+
+using namespace std;
+
+void testcase() {
+    int n; cin >> n;
+    int m; cin >> m;
+    
+    const int MAX_WEIGHT = 128;
+    
+    vector<int> c(n - 1);
+    for (int i = 0 ; i < n - 1 ; i++) {
+        cin >> c[i];
+    }
+    
+    graph G(n + 2);
+    const int src = n;
+    const int sink = n + 1;
+    edge_adder adder(G);
+    
+    int c_prev = 0;
+    for (int i = 0 ; i < n ; i++) {
+        int ci = 0;
+        if (i < n - 1) {
+            ci = c[i];
+            adder.add_edge(i, i + 1, ci, 0);
+            
+            if (ci > c_prev) {
+                // Outgoing flow is larger than incoming - we need to pull from src to compensate
+                adder.add_edge(src, i, ci - c_prev, 0);
+            }
+        }
+        
+        if (i > 0 && ci < c_prev) {
+            // There is an incoming flow, and it is larger than the outgoing flow - we send to sink to compensate
+            adder.add_edge(i, sink, c_prev - ci, 0);
+        }
+        
+        if (i == n - 1) {
+            // Last node - we send all to sink
+            adder.add_edge(i, sink, c_prev, 0);
+        }
+        
+        c_prev = ci;
+    }
+    
+    for (int i = 0 ; i < m ; i++) {
+        int a; cin >> a;
+        int b; cin >> b;
+        int d; cin >> d;
+        
+        adder.add_edge(a, b, 1, -d);
+    }
+    
+    int flow = boost::push_relabel_max_flow(G, src, sink);
+    boost::cycle_canceling(G);
+    int cost = boost::find_flow_cost(G);
+    
+    cout << -cost << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int t; cin >> t;
+    while (t--) testcase();
+    return 0;
+}
